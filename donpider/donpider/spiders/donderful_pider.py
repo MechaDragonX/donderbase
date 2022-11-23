cd1e454b82894df11fa250fac221d4758b3ca930
MechaDragonX <theofficialrv@gmail.com>
2022-11-22 17:02:07 -0800

    Scrape data with UTF-8 encoding to maintain special characters

--- donpider/donpider/spiders/donderful_pider.py
+++ donpider/donpider/spiders/donderful_pider.py
@@ -6,61 +6,151 @@ class DonderfulPider(scrapy.Spider):
         'https://dondafulfestival-20th.taiko-ch.net/en/music/songlist.php'
     ]
 
+
     def parse(self, response):
         for song in response.css('li.pops'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'pop_title': song.css('dt.song::text').getall(),
-                'pop_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'pop_title': titles,
+                'pop_subtitle': subtitles
             }
+
         for song in response.css('li.anime'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'anime_title': song.css('dt.song::text').getall(),
-                'anime_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'anime_title': titles,
+                'anime_subtitle': subtitles
             }
+
         for song in response.css('li.vocaloid'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'vocaloid_title': song.css('dt.song::text').getall(),
-                'vocaloid_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'vocaloid_title': titles,
+                'vocaloid_subtitle': subtitles
             }
 
         for song in response.css('li.variety'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'variety_title': song.css('dt.song::text').getall(),
-                'variety_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'variety_title': titles,
+                'variety_subtitle': subtitles
             }
+        
         for song in response.css('li.classic'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'classical_title': song.css('dt.song::text').getall(),
-                'classical_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'classic_title': titles,
+                'classic_subtitle': subtitles
             }
+
         for song in response.css('li.game'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'game_title': song.css('dt.song::text').getall(),
-                'game_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'game_title': titles,
+                'game_subtitle': subtitles
             }
+
         for song in response.css('li.namco'):
+            titles = song.css('dt.song::text').getall()
+            for title in titles:
+                title = title.encode('utf-8').decode('unicode-escape')
+
+            subtitles = list(map(
+                lambda x: x.replace('<dd>', ''), 
+                song.css('dd').getall()
+            ))
+            subtitles = list(map(
+                lambda x: x.replace('</dd>', ''), 
+                subtitles
+            ))
+            for subtitle in subtitles:
+                subtitle = subtitle.encode('utf-8').decode('unicode-escape')
+
             yield {
-                'namco_title': song.css('dt.song::text').getall(),
-                'namco_subtitle': list(map(
-                    lambda x: x.replace('<dd></dd>', ''), 
-                    song.css('dd').getall()
-                ))
+                'namco_title': titles,
+                'namco_subtitle': subtitles
             }
