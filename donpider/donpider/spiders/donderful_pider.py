c4e6fcbb250f8bb5894c4c7c11b506fe3c826ff9
MechaDragonX <theofficialrv@gmail.com>
2022-11-23 13:39:32 -0800

    Refactor a bit more and remove breaks in titles and subtitles

--- donpider/donpider/spiders/donderful_pider.py
+++ donpider/donpider/spiders/donderful_pider.py
@@ -6,12 +6,80 @@ class DonderfulPider(scrapy.Spider):
         f'file:////{__file__.removesuffix("donpider/donpider/spiders/donderful_pider.py")}data/donderful.html'
     ]
 
-    def extractTitles(self, song):
+    def parse(self, response):
+        for song in response.css('li.pops'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'pop_title': title,
+                'pop_subtitle': subtitle
+            }
+
+        for song in response.css('li.anime'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'anime_title': title,
+                'anime_subtitle': subtitle
+            }
+
+        for song in response.css('li.vocaloid'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'vocaloid_title': title,
+                'vocaloid_subtitle': subtitle
+            }
+
+        for song in response.css('li.variety'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'variety_title': title,
+                'variety_subtitle': subtitle
+            }
+        
+        for song in response.css('li.classic'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'classical_title': title,
+                'classical_subtitle': subtitle
+            }
+
+        for song in response.css('li.game'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'game_title': title,
+                'game_subtitle': subtitle
+            }
+
+        for song in response.css('li.namco'):
+            title = self.extract_title(song)
+            subtitle = self.extract_subtitle(song)
+
+            yield {
+                'namco_title': title,
+                'namco_subtitle': subtitle
+            }
+
+    def extract_title(self, song):
         titles = song.css('dt.song::text').getall()
         for title in titles:
             title = title.encode('utf-8').decode('unicode-escape')
-        return titles
-    def extractSubtitles(self, song):
+        
+        if len(titles) == 1:
+            return titles[0]
+        return self.remove_breaks(titles)
+
+    def extract_subtitle(self, song):
         subtitles = list(map(
                 lambda x: x.replace('<dd>', ''), 
                 song.css('dd').getall()
@@ -22,75 +90,17 @@ class DonderfulPider(scrapy.Spider):
             ))
         for subtitle in subtitles:
             subtitle = subtitle.encode('utf-8').decode('unicode-escape')
-        return subtitles
 
-    def parse(self, response):
-        for song in response.css('li.pops'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'pop_title': titles,
-                'pop_subtitle': subtitles
-            }
-
-        for song in response.css('li.anime'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'anime_title': titles,
-                'anime_subtitle': subtitles
-            }
-
-        for song in response.css('li.vocaloid'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'vocaloid_title': titles,
-                'vocaloid_subtitle': subtitles
-            }
-
-        for song in response.css('li.variety'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'variety_title': titles,
-                'variety_subtitle': subtitles
-            }
-        
-        for song in response.css('li.classic'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'classical_title': titles,
-                'classical_subtitle': subtitles
-            }
-
-        for song in response.css('li.game'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'game_title': titles,
-                'game_subtitle': subtitles
-            }
-
-        for song in response.css('li.namco'):
-            titles = self.extractTitles(song)
-
-            subtitles = self.extractSubtitles(song)
-
-            yield {
-                'namco_title': titles,
-                'namco_subtitle': subtitles
-            }
+        if len(subtitles) == 1:
+                return subtitles[0]
+        return self.remove_breaks(subtitles)
+    def remove_breaks(self, list):
+        result = ''
+        i = 0
+        while i < len(list):
+            if i != len(list) - 1:
+                result += f'{list[i]} '
+            else:
+                result += list[i]
+            i += 1
+        return result
