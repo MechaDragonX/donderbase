f28b6b377d537585490d0dc4ccd0bb4875572516
MechaDragonX <theofficialrv@gmail.com>
2023-06-15 16:57:37 -0700

    Actually implement using search with a param

--- donderbase/main.py
+++ donderbase/main.py
@@ -20,9 +20,17 @@ def is_command(input: str) -> bool:
             return True
 
 
-def search(search: Search) -> list:
-    # return search.search(input('> '))
-    print('検索')
+def handle_search(input_str: str):
+    params = input_str.split(' ')
+    if len(params) == 1 or params[0] != 'search':
+        return None
+    
+    return input_str[7:]
+
+
+def search(search: Search, input: str) -> list:
+    # return search.search(input)
+    print(f'検索 {input}')
 
 
 def exit() -> None:
@@ -33,24 +41,29 @@ def exit() -> None:
 print('Welcome to Donderbase! Please wait as all the data is loaded into the database!')
 
 # search = Search()
+# search.add(Wrangler.donderful_wrangle(Wrangler.file_import('donderful')))
 Wrangler.donderful_wrangle(Wrangler.file_import('donderful'))
-# search.add()
 
 print('Import successfull!')
 
 
 input_str = ''
+params = []
+query = None
 while(True):
     while(True):
         input_str = input('> ')
-        if is_command(input_str):
+        params = input_str.split(' ')
+        if is_command(params[0]):
             break
 
-    match input_str:
+    match params[0]:
         case 'help':
             print('UNIMPLEMENTED')
         case 'search':
-            search(search)
+            query = handle_search(input_str)
+            if query != None:
+                search(search, query)
         case 'exit':
             exit()
     
