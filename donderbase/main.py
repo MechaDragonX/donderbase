a6b5de34268aa09d2384315e1d067b9341d8d70a
MechaDragonX <theofficialrv@gmail.com>
2023-06-15 16:07:06 -0700

    Test command handling in CLI to main.py

--- donderbase/main.py
+++ donderbase/main.py
@@ -1,6 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
+from search import Search
 from wrangler import Wrangler
 
-donderful = Wrangler.donderful_wrangle(Wrangler.file_import('donderful'))
+
+commands = {
+    'help': '',
+    'search': 'Usage: "search <query>',
+    'exit': 'Exit the program'
+}
+
+
+def is_command(input: str) -> bool:
+    for key in commands:
+        if input == key:
+            return True
+
+
+def search(search: Search) -> list:
+    # return search.search(input('> '))
+    print('検索')
+
+
+def exit() -> None:
+    print('Thanks for using Donderbase!')
+    sys.exit()
+
+
+print('Welcome to Donderbase! Please wait as all the data is loaded into the database!')
+
+# search = Search()
+Wrangler.donderful_wrangle(Wrangler.file_import('donderful'))
+# search.add()
+
+print('Import successfull!')
+
+
+input_str = ''
+while(True):
+    while(True):
+        input_str = input('> ')
+        if is_command(input_str):
+            break
+
+    match input_str:
+        case 'help':
+            print('UNIMPLEMENTED')
+        case 'search':
+            search(search)
+        case 'exit':
+            exit()
+    
+    print()
