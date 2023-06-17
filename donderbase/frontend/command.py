deb21423fa03ce7ce379b57dd23364344f4e312c
MechaDragonX <theofficialrv@gmail.com>
2023-06-16 18:12:34 -0700

    Add messages for user error

--- donderbase/frontend/command.py
+++ donderbase/frontend/command.py
@@ -9,7 +9,8 @@ from search import SearchClient
 class Command:
     __commands = {
         'help': '',
-        'search': 'Usage: "search <field> <query>',
+        'search':
+            'Usage: "search <field> <query>\nThe valid fields are "title", "subtitle", "artist", "source", "genre", and "game".\nMake sure to type these in lowercase!',
         'exit': 'Exit the program'
     }
 
@@ -34,18 +35,20 @@ class Command:
             while(True):
                 input_str = input('> ')
                 params = input_str.split(' ')
-                if Command.__is_command(params[0]):
+                if not Command.__is_command(params[0]):
+                    Command.__not_found(params[0])
+                else:
                     break
 
             match params[0]:
                 case 'help':
                     print('UNIMPLEMENTED')
                 case 'search':
-                    query = Command.__handle_search(input_str)
+                    query = Command.__handle_search(params)
                     if query != None:
                         Command.__search(search_client, query)
                 case 'exit':
-                    Command.__exit()
+                    Command.__exit(params[0])
             
             print()
 
@@ -56,12 +59,12 @@ class Command:
                 return True
 
 
-    def __handle_search(input_str: str):
-        params = input_str.split(' ')
-
-        if (len(params) == 1 or params[0]) != 'search':
+    def __handle_search(params: list):
+        if len(params) == 1:
+            print('You have to specify what you want to search and what field to search!')
             return None
         if not (params[1] in Command.__search_params):
+            print('The field you specified does not exist!')
             return None
         
         return f'{params[1]}:{params[2]}'
@@ -69,9 +72,17 @@ class Command:
 
     def __search(search_client: SearchClient, input: str) -> list:
         result = search_client.search(input)
-        print(json.dumps(result, indent=4))
+
+        if len(result) == 0:
+            print('No results found!')
+        else:
+            print(json.dumps(result, indent=4))
 
 
     def __exit() -> None:
         print('Thanks for using Donderbase!')
         sys.exit()
+
+    
+    def __not_found(input: str) -> None:
+        print(f'The command "{input}" was not found!\n')
